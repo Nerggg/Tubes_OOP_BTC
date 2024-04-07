c4e56bc7ad919e91f71328367b461f113471bdd6
heisenberg <owentobias21@gmail.com>
2024-04-07 21:10:14 +0700

    feat: add wrongFoodException

--- src/lib/Exception.hpp
+++ src/lib/Exception.hpp
@@ -5,11 +5,22 @@
 
 using namespace std;
 
-class commandInvalidExeption {
-    public:
-        const char* what() {
-            return "Command is invalid! Please enter a valid command.";
-        };
+class commandInvalidExeption
+{
+public:
+    const char *what()
+    {
+        return "Command is invalid! Please enter a valid command.";
+    };
+};
+
+class wrongFoodException
+{
+public:
+    const char *what()
+    {
+        return "Jangan kasih makan itu dong, gabisa!";
+    };
 };
 
 #endif
