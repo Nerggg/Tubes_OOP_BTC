e5d5fb1f2d679423cea9231c72a1b296a149574a
heisenberg <owentobias21@gmail.com>
2024-04-08 23:46:01 +0700

    fix: exception and weight add

--- src/cmd/9_makan.cpp
+++ src/cmd/9_makan.cpp
@@ -9,6 +9,8 @@ void Player::makan()
     // Initialize slowprinter
     SlowPrinter &sc = *(SlowPrinter::getSlowPrinter());
     int totalFood = 0;
+    sc << "Pilih makanan dari penyimpanan" << endl;
+    cetakPenyimpanan();
     for (const auto &pair : inventory.getAllItems())
     {
         if (pair.second->isProduct())
@@ -26,38 +28,41 @@ void Player::makan()
     }
     else
     {
-        cetakPenyimpanan();
         string choice;
         while (true)
         {
-            sc << "Slot: ";
-            cin >> choice;
             Item *item = nullptr;
             try
             {
+                sc << "\nSlot: ";
+                cin >> choice;
                 item = inventory.getItem(choice);
             }
             catch (out_of_range &e)
             {
-                sc << BOLD RED << "Kamu mengambil harapan kosong dari penyimpanan." << endl;
+                sc << BOLD RED << "\nKamu mengambil harapan kosong dari penyimpanan." << endl;
                 sc << "Silahkan masukkan slot yang berisi makanan." << endl;
             }
             if (item->isBuilding())
             {
-                sc << BOLD RED << "Apa yang kamu lakukan?\?!! Kamu mencoba untuk memakan itu?!!" << endl;
-                sc << "Silahkan masukkan slot yang berisi makanan." << endl;
-            }
-            Product *produk = (Product *)item;
-            if (!produk->isEdibleAnimal() && !produk->isEdiblePlant())
-            {
-                sc << BOLD RED << "Apa yang kamu lakukan?\?!! Kamu mencoba untuk memakan itu?!!" << endl;
+                sc << BOLD RED << "\nApa yang kamu lakukan?\?!! Kamu mencoba untuk memakan itu?!!" << endl;
                 sc << "Silahkan masukkan slot yang berisi makanan." << endl;
             }
             else
             {
-                inventory.DeleteItemAt(choice);
-                sc << "Dengan lahapnya, kamu memakan hidangan itu" << endl;
-                sc << BOLD GREEN << "Alhasil, berat badan kamu naik menjadi " << this->weight << endl;
+                Product *produk = (Product *)item;
+                if (!produk->isEdibleAnimal() && !produk->isEdiblePlant())
+                {
+                    sc << BOLD RED << "\nApa yang kamu lakukan?\?!! Kamu mencoba untuk memakan itu?!!" << endl;
+                    sc << "Silahkan masukkan slot yang berisi makanan." << endl;
+                }
+                else
+                {
+                    this->weight = this->weight + produk->getAddedWeight();
+                    inventory.DeleteItemAt(choice);
+                    sc << "\nDengan lahapnya, kamu memakan hidangan itu" << endl;
+                    sc << BOLD GREEN << "Alhasil, berat badan kamu naik menjadi " << this->weight << endl;
+                }
             }
         }
     }
